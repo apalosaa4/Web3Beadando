b4e37556cb719b49e7fe01fa4a432576cf4d0909
Viktoria Bognar <apalosaa4@gmail.com>
2022-05-10 21:59:30 +0200

    RecipeFreeFrom table added

---
+++ WebRecepies/app/Http/Controllers/RecipeFreeFromController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RecipeFreeFrom;
+use App\Http\Requests\StoreRecipeFreeFromRequest;
+use App\Http\Requests\UpdateRecipeFreeFromRequest;
+
+class RecipeFreeFromController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreRecipeFreeFromRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRecipeFreeFromRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RecipeFreeFrom  $recipeFreeFrom
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RecipeFreeFrom $recipeFreeFrom)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\RecipeFreeFrom  $recipeFreeFrom
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RecipeFreeFrom $recipeFreeFrom)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateRecipeFreeFromRequest  $request
+     * @param  \App\Models\RecipeFreeFrom  $recipeFreeFrom
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateRecipeFreeFromRequest $request, RecipeFreeFrom $recipeFreeFrom)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RecipeFreeFrom  $recipeFreeFrom
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RecipeFreeFrom $recipeFreeFrom)
+    {
+        //
+    }
+}
