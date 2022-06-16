e08ca94b554ba8c368c52c5efe8464fb708ad9e0
Viktoria Bognar <apalosaa4@gmail.com>
2022-06-16 15:15:41 +0200

    Recipe by id view added

--- WebRecepies/resources/views/recepie.blade.php
+++ WebRecepies/resources/views/recepie.blade.php
@@ -3,46 +3,24 @@
 @section('content')
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-white-100 dark:bg-white-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-white-700 dark:text-white-500 underline">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-white-700 dark:text-white-500 underline">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-white-700 dark:text-white-500 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="container" style="margin-top: 10px">
-                <div>Each recepies come here</div>
-                    <table class="table table-striped mt-5">
-                        <div class="row">
-                            <thead>
-                                <tr>
-                                    <th scope="col-sm">Recipe Name</th>
-                                    <th scope="col-sm">User name</th>
-                                    <th scope="col-sm">Free From</th>
-                                    <th scope="col-sm">Description</th>
-                                </tr>
-                                @foreach ($recepies as $recepie)
-                                <tr>
-                                    <th scope="col-sm">{{ $recepie->recipe_name }}</th>
-                                    <th scope="col-sm">{{ $recepie->user_id }}</th>
-                                    <th scope="col-sm">{{ $recepie->freefrom }}</th>
-                                    <th scope="col-sm">
-                                        <a href={{"recepie/".$recepie['recipe_id']}}>
-                                            <button class="btn btn-dark" type="button">Read more..</button>
-                                        </a>
-                                    </th>
-                                </tr>
-                                @endforeach
-                            </thead>
+                <div style="text-align:center"><h4 style="color: rgba(140, 63, 187, 0.892)">You can search here between hundreds of different recepies you could easily make at home!</h4></div> <br><br>
+                
+                <div class="row">
+                    @foreach ($recepies as $recepie)
+                    <div class="col-sm-4">
+                        <div class="card">
+                          <div class="card-body">
+                            <h5 class="card-title">{{ $recepie->recipe_name }}</h5>
+                            <p class="card-text">{{ $recepie->freefrom }}</p>
+                            <a href={{"recepie/".$recepie['recipe_id']}}>
+                                <button class="btn btn-primary" style="background-color: rgba(140, 63, 187, 0.892); border-color:rgba(140, 63, 187, 0.892)" type="button">Description</button>
+                            </a>
+                          </div>
                         </div>
-                    </table>
+                    </div>
+                    @endforeach
+                </div>
             </div>
         </div>
     </body>
--- WebRecepies/resources/views/recepieid.blade.php
+++ WebRecepies/resources/views/recepieid.blade.php
@@ -1,30 +1,34 @@
 @extends('layouts.app')
 
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@section('content')
     <body class="antialiased">
+        <div class="relative flex items-top justify-center min-h-screen bg-white-100 dark:bg-white-900 sm:items-center py-4 sm:pt-0">
             <div class="container" style="margin-top: 10px">
-                <div>Each recepies come here</div>
-                    <table class="table table-striped mt-5">
-                        <div class="row">
-                            <thead>
-                                <tr>
-                                    <th scope="col-sm">Recipe Name</th>
-                                    <th scope="col-sm">User name</th>
-                                    <th scope="col-sm">Free From</th>
-                                    <th scope="col-sm">Description</th>
-                                </tr>
-                                <!-- idk itt a recepie helyere mi kene -->
-                                @foreach ($recepie as $recepies)
-                                <tr>
-                                    <th scope="col-sm">{{ $recepies->recipe_name }}</th>
-                                    <th scope="col-sm">{{ $recepies->user_id }}</th>
-                                    <th scope="col-sm">{{ $recepies->freefrom }}</th>
-                                </tr>
-                                @endforeach
-                            </thead>
-                        </div>
-                    </table>
+                <div style="text-align:center"><h4 style="color: rgba(140, 63, 187, 0.892)">Hope You have fun while making the {{ $recepie->recipe_name }}!</h4></div> <br><br>
+                <div class="row">
+                    <thead>
+                        <tr>
+                            <h1 style="color: rgba(140, 63, 187, 0.892)"><th scope="col-sm">{{ $recepie->recipe_name }}</th></h1>            
+                        </tr>
+                    </thead>
+                </div>
+                <br>    
+                <div class="row">
+                    <thead>
+                        <tr>
+                            <h3 style="color: rgba(53, 137, 206, 0.687)"><th scope="col-sm">{{ $recepie->freefrom }}</th></h3>
+                        </tr>
+                    </thead>
+                </div> 
+                <br>  
+                <div class="row">
+                    <thead>
+                        <tr>
+                            <h5>{{ $recepie->description }}</th></h5>
+                        </tr>
+                    </thead>
+                </div>      
             </div>
         </div>
     </body>
-</html>
+@endsection
